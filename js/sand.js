36c7024790fc16f0ce1927449963e1eaea38324b
Devcord <sasn026@gmail.com>
2021-02-15 01:19:02 +0300

    new

---
+++ js/sand.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.co').click(function(){
+        $('label').text("Check Your email")
+        $('#em').addClass('active')
+        $('input').addClass('active')
+        $('a').addClass('active')
+    })
+})
